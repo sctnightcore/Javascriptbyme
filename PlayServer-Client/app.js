033a0dff8860d2cc3359bd2013743314a70e73cf
sctnightcore <nightcoremax@gmail.com>
2019-03-13 15:35:41 +0700

    add todo

--- PlayServer-Client/app.js
+++ PlayServer-Client/app.js
@@ -1,9 +1,12 @@
 const axios = require('axios');
+const axiosSaveFile = require('axios-savefile');
+const fs = require('fs');
 
 main();
 
 function main() {
-	getimgpic();
+	let checksum = getimgpic();
+	console.log(checksum);
 }
 
 async function getimgpic() {
@@ -12,6 +15,10 @@ async function getimgpic() {
 		if (response.status == 200 ) {
 			console.log("GetImg:" +response.data.checksum+".png\n");
 			var checksum = response.data.checksum;
+			// TODO Save img to /img/
+			return checksum;
+		} else {
+			console.log("Cannot get img from playserver\n");
 		}
 	})
 	.catch((error) => {
