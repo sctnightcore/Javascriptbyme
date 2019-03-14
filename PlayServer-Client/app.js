dba68e94cbb29714b7807f2a645a643f96a288fa
sctnightcore <nightcoremax@gmail.com>
2019-03-14 21:13:15 +0700

    Update code !

--- PlayServer-Client/app.js
+++ PlayServer-Client/app.js
@@ -3,30 +3,26 @@ const axiosFile = require('axios-file');
 
 main();
 
-function main() {
-	getimgpic();
-}
-
-async function getimgpic() {
-  	let res = await axios.post('http://playserver.co/index.php/Vote/ajax_getpic/PserverN-15282');
-  	if (res.status == 200 ) {
-  		var checksum = res.data.checksum;
-  		console.log("GetImg:" +checksum+".png");
-  		saveimg(checksum);
-  		return checksum;
- 	} else {
- 		console.log("Cannot get img from PlayServer");
- 	}
+async function main() {
+	await banner();
+	await getimgpic();
 }
 
 
-async function saveimg(checksum) {
-  	let savefile = await axiosFile({
-  		url: 'http://playserver.co/index.php/VoteGetImage/'+checksum,
-  		method: 'get',
-  		savePath: `img/${checksum}.png`
+function banner() {
+	console.log("=========================");
+	console.log("PlayServer-Javascript");
+	console.log("=========================");
+}
+
+function getimgpic() {
+	axios.post('http://playserver.co/index.php/Vote/ajax_getpic/PserverN-15282')
+	.then(resp => {
+	  	var checksum = resp.data.checksum;
+	  	console.log("GetImg:" +checksum+".png");
+	  	axiosFile({url: `http://playserver.co/index.php/VoteGetImage/${checksum}`,method: 'get',savePath: `img/${checksum}.png`});
+	  	console.log("SaveImg " + checksum + " has been downloaded!");
+	  	return checksum;
   	});
-  	console.log("SaveImg " + checksum + " has been downloaded!");
 }
-
 
